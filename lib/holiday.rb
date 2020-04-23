62740c8310b087dd00dbee493c5fa90bb6a5ccff
Koray Ozkal <koryozkal@gmail.com>
2020-04-23 06:39:51 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -81,7 +81,13 @@ end
 def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists
   # include the string "BBQ"
-  
+  holiday_supplies.collect do |season, holidays|
+
+    holidays.collect do |holiday, supplies|
+      if supplies.include?("BBQ")
+         holidays_with_BBQ << holiday 
+      end
+    end
 
 
 end
